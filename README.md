4529e6de5f2a1e55d3349083eec2144137d2f988
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2024-05-26 11:46:42 -0300

    FEAT: initial settings and structure

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # duckDB-Lab
-First tests with DuckDB
+
+### Goal
+
+The objective of this test is to develop a data ingestion pipeline using Python, which will:
+
+* **Extract** data on the Gross Domestic Product (GDP) of South American countries using the World Bank API:
+  * Endpoint: `https://api.worldbank.org/v2/country/ARG;BOL;BRA;CHL;COL;ECU;GUY;PRY;PER;SUR;URY;VEN/indicator/NY.GDP.MKTP.CD?format=json&page=1&per_page=50`
+
+* **Load** this data into a SQL database of your choice (such as PostgreSQL, SQLite, DuckDB, Trino, etc.):
+  * Create `country` (id, name, iso3_code) and `gdp` (country_id, year, value) tables.
+  * Additional structures or control columns may be implemented as needed.
+
+* **Query** the loaded data to produce a pivoted report of the last 5 years for each country, presented in Billions:
+  * Expected query structure:
+
+    | id | name     | iso3_code | 2019 | 2020 | 2021 | 2022 | 2023 |
+    |----|----------|-----------|------|------|------|------|------|
---
+++ utils/logs/main.py
@@ -0,0 +1,33 @@
+import logging
+
+class Logger:
+    """
+        This class it will responsable for logs manipulations and settings
+    """
+
+    def __init__(self, module:str = __name__):
+        self.logger = logging.getLogger(module)
+ 
+    #@staticmethod
+    def emit(self, message:str, category:str = 'INFO'):
+        # Define a particular log format
+        log_format = "[%(asctime)s] - [%(name)s] - [%(levelname)s] - %(message)s"
+        #formatter = logging.Formatter(log_format, datefmt="%Y-%m-%d %H:%M:%S")
+
+        logging.basicConfig(level=logging.DEBUG, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
+        
+        # Separe output messages, due a log category
+        try:
+            if category == 'INFO':
+                self.logger.info(message)
+            elif category == 'DEBUG':
+                self.logger.debug(message)
+            elif category == 'WARNING':
+                self.logger.warning(message)
+            elif category == 'CRITICAL':
+                self.logger.warning(message)
+            elif category == 'ERROR':
+                self.logger.error(message)
+        except Exception as e:
+            raise ValueError(f'We have found the following error when trying to set logger category type: {e}')
+        
